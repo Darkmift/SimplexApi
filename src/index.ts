4784b24e7950295ab51131e4fe8aa9c76f5c25ed
darkmift <darkmift@gmail.com>
2022-11-23 18:25:43 +0200

    final edit of main index

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express, NextFunction, Request, Response } from 'express';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
 import { NODE_ENV, PORT, LOG_FORMAT } from '@config';
 import validateEnv from './utils/validateEnv';
 import { logger, stream } from '@utils/logger';
+import { v4 as uuidv4 } from 'uuid';
+import mainRouter from './main.router';
 
 validateEnv();
 
@@ -13,9 +15,12 @@ const port = PORT || 5000;
 
 app.use(morgan(LOG_FORMAT, { stream }));
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Express + TypeScript Server');
+app.all('*', (req: Request, res: Response, next: NextFunction) => {
+    req.uuid = uuidv4();
+    next();
 });
+
+app.use('/api', mainRouter);
 
 app.listen(port, () => {
     console.log(`⚡️[NODE_ENV]: ${NODE_ENV}`);
